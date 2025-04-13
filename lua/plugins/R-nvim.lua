2fd5ec3be811c768716524525e06c9f46d9299d3
qifei <qifei9@gmail.com>
2025-04-14 01:06:46 +0800

    Add use cmp-r for completion in R

--- lua/plugins/R-nvim.lua
+++ lua/plugins/R-nvim.lua
@@ -5,6 +5,39 @@ return {
     opts = {
       -- Create a table with the options to be passed to setup()
       R_args = { "--quiet", "--no-save" },
+      hook = {
+        on_filetype = function()
+          -- This function will be called at the FileType event
+          -- of files supported by R.nvim. This is an
+          -- opportunity to create mappings local to buffers.
+          -- vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", { buffer = true })
+          -- vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", { buffer = true })
+
+          vim.api.nvim_buf_set_keymap(0, "n", "<localleader>d", "<Plug>RDSendLine", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<localleader>pe", "<Plug>RSendParagraph", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<localleader>pa", "<Plug>RDSendParagraph", {})
+          vim.api.nvim_buf_set_keymap(0, "v", "<localleader>se", "<Plug>RSendSelection", {})
+
+          local wk = require("which-key")
+          wk.add({
+            buffer = true,
+            mode = { "n", "v" },
+            { "<localleader>a", group = "all" },
+            { "<localleader>b", group = "between marks" },
+            { "<localleader>c", group = "chunks" },
+            { "<localleader>f", group = "functions" },
+            { "<localleader>g", group = "goto" },
+            { "<localleader>i", group = "install" },
+            { "<localleader>k", group = "knit" },
+            { "<localleader>p", group = "paragraph" },
+            { "<localleader>q", group = "quarto" },
+            { "<localleader>r", group = "r general" },
+            { "<localleader>s", group = "split or send" },
+            { "<localleader>t", group = "terminal" },
+            { "<localleader>v", group = "view" },
+          })
+        end,
+      },
       -- pdfviewer = "",
       R_app = "radian",
       R_cmd = "R",
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -10,13 +10,45 @@
 -- * override the configuration of LazyVim plugins
 return {
   {
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = "*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
+  {
+    -- use cmp-r for completion in R, add the following to ~/.Rprofile
+    -- options(
+    --   languageserver.server_capabilities =
+    --     list(
+    --       completionProvider = FALSE,
+    --       completionItemResolve = FALSE
+    --     )
+    -- )
     "saghen/blink.cmp",
+    dependencies = { "R-nvim/cmp-r" },
     opts = {
       keymap = {
         preset = "enter",
         -- fix tab do not work issue 2025-04-11
         ["<S-Tab>"] = { "select_prev", "fallback" },
         ["<Tab>"] = { "select_next", "fallback" },
+      },
+      sources = {
+        -- adding any nvim-cmp sources here will enable them
+        -- with blink.compat
+        compat = {},
+        default = { "lsp", "path", "snippets", "buffer", "cmp_r" },
+        providers = {
+          -- create provider
+          cmp_r = {
+            -- IMPORTANT: use the same name as you would for nvim-cmp
+            name = "cmp_r",
+            module = "blink.compat.source",
+          },
+        },
       },
     },
   },
