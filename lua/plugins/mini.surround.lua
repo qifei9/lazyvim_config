e4de54f02adbb2fea60c788801f51f588f01c530
qifei <qifei9@gmail.com>
2025-04-14 01:39:25 +0800

    Change keymap for add surround

---
+++ lua/plugins/mini.surround.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "<A-s>",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+}
